3fd18b414c763df8438ac71e0db31dcdbb07b3a7
Koesob <epi3706@khu.ac.kr>
2025-07-25 05:18:20 +0900

    Reward에 안내 추가

--- Assets/Scripts/View/ViewObject/RewardView.cs
+++ Assets/Scripts/View/ViewObject/RewardView.cs
@@ -6,7 +6,9 @@ namespace Minomino
     public class RewardView : MonoBehaviour
     {
         [Header("이미지")]
-        public Image Image_Reward;
+        [SerializeField] private Image Image_Reward;
+        [SerializeField] private GameObject Object_Alert;
+        private bool isActiveReward = false;
 
         public void SetTransparency(bool isTransparent)
         {
@@ -14,6 +16,7 @@ namespace Minomino
 
             Color color = Image_Reward.color;
             color.a = isTransparent ? 0f : 1f; // 투명도 설정
+            isActiveReward = !isTransparent; // 활성화 상태 업데이트
             Image_Reward.color = color;
         }
 
@@ -27,6 +30,18 @@ namespace Minomino
             {
                 Image_Reward.sprite = GlobalSettings.Instance.Sprite_Reward_Alert;
             }
+        }
+
+        public void OnPointerEnter()
+        {
+            if (!isActiveReward) return;
+            Object_Alert.SetActive(true);
+        }
+
+        public void OnPointerExit()
+        {
+            if (!isActiveReward) return;
+            Object_Alert.SetActive(false);
         }
     }
 }
